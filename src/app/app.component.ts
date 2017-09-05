b44b092e6acccea62c3ac101bba77215cd920fc2
Ginny Guo <harry.yuetu@126.com>
2017-09-05 18:40:18 +0800

    Version 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Simulation Tool';
-  hero = 'Windstorm';
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
