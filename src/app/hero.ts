029207561ff90f3c3f374acaa11d16cd1b46ba50
Ginny Guo <harry.yuetu@126.com>
2017-09-19 18:29:20 +0800

    change class

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,22 @@
 export class Hero {
   id: number;
   name: string;
+  tradeDate: Date;
+  giveIn: string;
+  execFirm: string;
+  qty: number;
+  price: number;
+  type: string;
+  strike: number;
+  callPut: string;
+  tradeType: string;
+  trasationType: number;
+  tradeId: string;
+  exchange: string;
+  subFirmD: string;
+  subFirmT: string;
+  deliveryDate: Date;
+  side: string;
+  execAC: string;
+  isSelect: boolean;
 }
